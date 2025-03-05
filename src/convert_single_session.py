5434d46c82673536cdcf46c92a910a4f89200bc1
Manish <manishm@smaug.datalad.org>
2025-03-05 17:04:29 -0500

    Able to add electrodes info directly from raw file

--- src/convert_single_session.py
+++ src/convert_single_session.py
@@ -1,5 +1,6 @@
 import manimoh_nwb_converters as mnc
 
-input_dir = 'E:\\odor-pixels\\fromHector\\NoReward\\M541\\M541-2024-08-31'
-output_nwb_filepath = 'E:\\odor-pixels\\fromHector\\NoReward\\M541\\M541-2024-08-31\\test_2024-08-31.nwb'
-mnc.create_nwb_file(input_dir, output_nwb_filepath, overwrite=True)
+input_dir = '/mnt/datasets/incoming/mvdm/OdorSequence/sourcedata/preprocessed/M541/M541-2024-08-31'
+# output_nwb_filepath = '/mnt/datasets/incoming/mvdm/OdorSequence/sourcedata/preprocessed/M541/M541-2024-08-31/M541-2024-08-31.nwb'
+output_nwb_filepath = '/home/manishm/test_metadata_M541-2024-08-31.nwb'
+mnc.create_nwb_file(input_dir, output_nwb_filepath)
